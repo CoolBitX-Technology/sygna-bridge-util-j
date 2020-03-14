0229e31b634a5e9dc4a202a56d26980b5cf42873
kunming.liu <kunming@coolbitx.com>
2020-03-14 21:00:41 +0800

    add checkSignData to validate if data to sign is valid

--- src/main/java/com/coolbitx/sygna/bridge/model/Transaction.java
+++ src/main/java/com/coolbitx/sygna/bridge/model/Transaction.java
@@ -4,28 +4,31 @@ import com.coolbitx.sygna.util.Validator;
 
 public class Transaction extends Packet {
 
-	private String transfer_id;
-	private String txid;
+    private String transfer_id;
+    private String txid;
 
-	public Transaction(String transfer_id, String txid, String signature) {
-		super(signature);
-		this.transfer_id = transfer_id;
-		this.txid = txid;
-	}
+    public Transaction(String transfer_id, String txid, String signature) {
+        super(signature);
+        this.transfer_id = transfer_id;
+        this.txid = txid;
+    }
 
-	public String getTransfer_id() {
-		return transfer_id;
-	}
+    public String getTransfer_id() {
+        return transfer_id;
+    }
 
-	public String getTxid() {
-		return txid;
-	}
-	
-	@Override
-	public void check() throws Exception {
-		super.check();
-		Validator.validateTransferId(transfer_id);
-		Validator.validateTxid(txid);
-	}
+    public String getTxid() {
+        return txid;
+    }
 
+    @Override
+    public void check() throws Exception {
+        super.check();
+        checkSignData();
+       
+    }
+    public void checkSignData() throws Exception {
+        Validator.validateTransferId(transfer_id);
+        Validator.validateTxid(txid);
+    }
 }
